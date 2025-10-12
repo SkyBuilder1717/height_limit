0044ab8e4b9fd1c536f020b14371cbfa4edce7f2
Ludus <alessio.desantis28@gmail.com>
2025-10-13 00:44:51 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -12,35 +12,30 @@ core.register_on_placenode(function(pp, node, p, _, item)
     local name = p:get_player_name()
     local limit = height_limit.limit
     local lmt = limit + 1
+
     if string.find(node.name, "mesecon") and height_limit.deny_mesecons then
         if (pp.y > (limit - (height_limit.mesecons_limit - 1))) and (pp.y < lmt) then
             core.remove_node(pp)
-            local color = "yellow"
-            if math.random(1, 2) == 2 then
-                color = "orange"
-            end
-            local msg = "Nuh Uh."
-            if math.random(1, 2) == 2 then
-                msg = "Nope!"
-            end
+            local color = math.random(1, 2) == 2 and "orange" or "yellow"
+            local msg = math.random(1, 2) == 2 and "Nope!" or "Nuh Uh."
             core.chat_send_player(name, core.colorize(color, S(msg)))
             return item
         end
     end
+
     if pp.y < lmt then
         return
     end
+
     core.chat_send_player(name, core.colorize("red", S("Sorry, but you cannot place higher than @1 blocks!", limit)))
     return item
 end)
 
 local function remove(pos, size)
-    local x1 = pos.x - size
-    local x2 = pos.x + size
-    local y1 = height_limit.limit + 1
-    local y2 = y1 + size
-    local z1 = pos.z - size
-    local z2 = pos.z + size
+    local x1, x2 = pos.x - size, pos.x + size
+    local y1, y2 = height_limit.limit + 1, height_limit.limit + 1 + size
+    local z1, z2 = pos.z - size, pos.z + size
+
     for x = x1, x2 do
         for y = y1, y2 do
             for z = z1, z2 do
@@ -56,7 +51,8 @@ core.register_globalstep(function()
         local pos = player:get_pos()
         local limit = height_limit.limit
         local lmt = limit + 1
-        if (pos.y > (limit - (height_limit.mesecons_limit - 1))) then
+
+        if pos.y > (limit - (height_limit.mesecons_limit - 1)) then
             remove(pos, height_limit.mesecons_limit)
         end
     end
