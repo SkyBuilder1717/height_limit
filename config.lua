5a206f21c61a24be9c3b12ed9eb0c2b579727c2b
Ludus <alessio.desantis28@gmail.com>
2025-10-13 16:34:43 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,13 @@
+return {
+    -- Altezza massima di costruzione
+    build_limit = 310,
+
+    -- Area di controllo rimozione mesecons sotto il limite
+    mesecons_limit = 20,
+
+    -- Impedisci il posizionamento di nodi "mesecon"
+    deny_mesecons = true,
+
+    -- Intervallo in secondi per il controllo automatico nel globalstep
+    check_interval = 2,
+}
--- init.lua
+++ init.lua
@@ -1,59 +1,112 @@
 local modname = core.get_current_modname()
-
-height_limit = {
-    limit = core.settings:get(modname) or 310,
-    mesecons_limit = core.settings:get(modname .. "_mesecons_close") or 20,
-    deny_mesecons = core.settings:get_bool(modname .. "_mesecons", true)
-}
-
+local modpath = core.get_modpath(modname)
+local cfg = dofile(modpath .. "/config.lua")
+local storage = minetest.get_mod_storage()
 local S = core.get_translator(modname)
 
-core.register_on_placenode(function(pp, node, p, _, item)
-    local name = p:get_player_name()
-    local limit = height_limit.limit
-    local lmt = limit + 1
-
-    if string.find(node.name, "mesecon") and height_limit.deny_mesecons then
-        if (pp.y > (limit - (height_limit.mesecons_limit - 1))) and (pp.y < lmt) then
-            core.remove_node(pp)
-            local color = math.random(1, 2) == 2 and "orange" or "yellow"
-            local msg = math.random(1, 2) == 2 and "Nope!" or "Nuh Uh."
-            core.chat_send_player(name, core.colorize(color, S(msg)))
-            return item
-        end
+-- Load saved height limit or default
+local function load_limit()
+    local val = tonumber(storage:get_string("build_limit"))
+    if val then
+        core.log("action", "[height_limit] Loaded saved build limit: " .. val)
+        return val
     end
+    return cfg.build_limit
+end
 
-    if pp.y < lmt then
-        return
+-- Save new limit
+local function save_limit(limit)
+    storage:set_string("build_limit", tostring(limit))
+    core.log("action", "[height_limit] New build limit saved: " .. limit)
+end
+
+cfg.build_limit = load_limit()
+
+local function is_mesecon(name)
+    return name and string.find(name, "mesecon")
+end
+
+local function exceeds_limit(pos)
+    if not pos or not pos.y then
+        return false
     end
+    return (pos.y + 1.0) > cfg.build_limit
+end
 
-    core.chat_send_player(name, core.colorize("red", S("Sorry, but you cannot place higher than @1 blocks!", limit)))
-    return item
-end)
+local function send_mesecon_denied_message(player_name)
+    local colors = {"yellow", "orange"}
+    local msgs = {S("Nope!"), S("Nuh uh."), S("Not here!")}
+    local color = colors[math.random(#colors)]
+    local msg = msgs[math.random(#msgs)]
+    core.chat_send_player(player_name, core.colorize(color, msg))
+end
 
-local function remove(pos, size)
-    local x1, x2 = pos.x - size, pos.x + size
-    local y1, y2 = height_limit.limit + 1, height_limit.limit + 1 + size
-    local z1, z2 = pos.z - size, pos.z + size
+local function send_height_limit_message(player_name, limit)
+    local msg = S("Sorry, but you cannot place higher than @1 blocks!", limit)
+    core.chat_send_player(player_name, core.colorize("red", msg))
+end
 
-    for x = x1, x2 do
-        for y = y1, y2 do
-            for z = z1, z2 do
-                local cpos = {x = x, y = y, z = z}
-                core.remove_node(cpos)
-            end
+local function remove_above_limit(pos)
+    if not pos or not pos.y then return end
+    local start_y = cfg.build_limit + 1
+    local end_y = math.floor(pos.y + cfg.mesecons_limit)
+    for y = start_y, end_y do
+        local cpos = {x = pos.x, y = y, z = pos.z}
+        local node = core.get_node_or_nil(cpos)
+        if node and node.name and node.name ~= "air" then
+            core.remove_node(cpos)
         end
     end
 end
 
-core.register_globalstep(function()
-    for _, player in pairs(core.get_connected_players()) do
-        local pos = player:get_pos()
-        local limit = height_limit.limit
-        local lmt = limit + 1
+-- Node placement event
+core.register_on_placenode(function(pos, node, placer, pointed_thing, itemstack)
+    if not placer or not pos then return end
+    local name = placer:get_player_name()
 
-        if pos.y > (limit - (height_limit.mesecons_limit - 1)) then
-            remove(pos, height_limit.mesecons_limit)
+    if cfg.deny_mesecons and is_mesecon(node.name) then
+        if pos.y > (cfg.build_limit - (cfg.mesecons_limit - 1)) and pos.y < cfg.build_limit + 1 then
+            core.remove_node(pos)
+            send_mesecon_denied_message(name)
+            return itemstack
+        end
+    end
+
+    if exceeds_limit(pos) or (pointed_thing and pointed_thing.above and exceeds_limit(pointed_thing.above)) then
+        send_height_limit_message(name, cfg.build_limit)
+        core.remove_node(pos)
+        return itemstack
+    end
+end)
+
+-- Periodic cleanup
+local timer = 0
+core.register_globalstep(function(dtime)
+    timer = timer + dtime
+    if timer < cfg.check_interval then return end
+    timer = 0
+
+    for _, player in ipairs(core.get_connected_players()) do
+        local pos = vector.round(player:get_pos())
+        if pos.y > (cfg.build_limit - cfg.mesecons_limit) then
+            remove_above_limit(pos)
         end
     end
 end)
+
+-- Admin command
+core.register_chatcommand("set_height_limit", {
+    params = "<number>",
+    description = S("Set a new height limit for building."),
+    privs = {server = true},
+    func = function(name, param)
+        local limit = tonumber(param)
+        if not limit then
+            return false, S("Invalid value. Usage: /set_height_limit <number>")
+        end
+        cfg.build_limit = limit
+        save_limit(limit)
+        core.chat_send_all(S("Build limit updated to @1 by @2", limit, name))
+        return true, S("New limit set to @1 (saved permanently)", limit)
+    end
+})
